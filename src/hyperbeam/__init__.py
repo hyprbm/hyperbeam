3b466a608378c2404970111014dd5b662783cb7e
Sean McCurdy <seanmccrdy@gmail.com>
2025-05-27 01:43:15 -0700

    updated version

--- src/hyperbeam/__init__.py
+++ src/hyperbeam/__init__.py
@@ -1,7 +1,7 @@
 from .web_search import ddgs_scraperapi_patch
 from .web_search import web_search
 
-__version__ = "0.1.2"  # Start with an initial version
+__version__ = "0.1.3"  # Start with an initial version
 
 __all__ = [
     "web_search",
